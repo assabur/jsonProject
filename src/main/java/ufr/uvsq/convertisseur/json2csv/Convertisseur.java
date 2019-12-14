9830bddcdcb1164aecb4875897965efa8394cada
RachData <rachidderra@gmail.com>
2019-12-14 20:31:17 +0000

    gestion de l'excetions saisies

--- src/main/java/ufr/uvsq/convertisseur/json2csv/Convertisseur.java
+++ src/main/java/ufr/uvsq/convertisseur/json2csv/Convertisseur.java
@@ -1,7 +1,11 @@
 package ufr.uvsq.convertisseur.json2csv;
 
 import java.io.File;
+import java.io.IOException;
 
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.MappingIterator;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,36 +13,48 @@ import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.dataformat.csv.CsvMapper;
 import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 
+import ufr.uvsq.convertisseur.Exceptions.*;
+
 public class Convertisseur
 
 {
 	
-
-   
-	
-	  public void to_csv (String chemin) 
+	  public void to_csv (String chemin) throws CheminsExceptions,FichiersVide
 	{
+		  //Verification de l'existance du fichier
+		  File Fichier=new File(chemin);
+		  if(Fichier.exists()==false){
+			  throw new CheminsExceptions();
+		  //if(true){
+		  }else{
+			  
+			  if(Fichier.length()==0){
+				  throw new FichiersVide();
+			  }else{
+				  try {
+
+						JsonNode jsonTree = new ObjectMapper().readTree(Fichier);
+						com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder csvSchemaBuilder = CsvSchema.builder();
+						
+						JsonNode firstObject = jsonTree.elements().next();
+						
+						firstObject.fieldNames().forEachRemaining(fieldName -> {((com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder) csvSchemaBuilder).addColumn(fieldName);} );
+						CsvSchema csvSchema = ((CsvSchema) csvSchemaBuilder.build()).withHeader();
+						
+						CsvMapper csvMapper = new CsvMapper();
+						csvMapper.writerFor(JsonNode.class)
+							  .with(csvSchema)
+							  .writeValue(new File("C:/Users/ACER E1/Documents/java/Convert_Json_Csv/jsonProject/src/main/ressources/Json_to_Csv/csvFrom_Json.csv"), jsonTree);
+						
+						
+					} catch (Exception e)
+					{
+						e.printStackTrace();
+					}
+			  }
+			  
+		  }
 		
-		try {
-			
-			JsonNode jsonTree = new ObjectMapper().readTree(new File(chemin));
-			
-			com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder csvSchemaBuilder = CsvSchema.builder();
-			
-			JsonNode firstObject = jsonTree.elements().next();
-			firstObject.fieldNames().forEachRemaining(fieldName -> {((com.fasterxml.jackson.dataformat.csv.CsvSchema.Builder) csvSchemaBuilder).addColumn(fieldName);} );
-			CsvSchema csvSchema = ((CsvSchema) csvSchemaBuilder.build()).withHeader();
-			
-			CsvMapper csvMapper = new CsvMapper();
-			csvMapper.writerFor(JsonNode.class)
-			  .with(csvSchema)
-			  .writeValue(new File("F:/EclipseProjects/json2csv/src/main/ressources/Fichier_Json_2_Csv"), jsonTree);
-			
-			
-		} catch (Exception e)
-		{
-			e.printStackTrace();
-		}
 			
 	}
 	
@@ -49,20 +65,19 @@ public class Convertisseur
 		{
 			CsvSchema orderLineSchema = CsvSchema.emptySchema().withHeader();
 			CsvMapper csvMapper = new CsvMapper();
-			MappingIterator<OrderLine> orderLines = csvMapper.readerFor(OrderLine.class)
+			MappingIterator<Structure> orderLines = csvMapper.readerFor(Structure.class)
 			  .with(orderLineSchema)
 			  .readValues(new File(chemin));
 			
 			
 			new ObjectMapper()
 			  .configure(SerializationFeature.INDENT_OUTPUT, true)
-			  .writeValue(new File("src/main/ressources/Fichier_Csv_2_Json"), orderLines.readAll());
+			  .writeValue(new File("C:/Users/ACER E1/Documents/java/Convert_Json_Csv/jsonProject/src/main/ressources/Csv_to_Json"), orderLines.readAll());
 		}
 		 catch (Exception e)
 		{
 			 e.printStackTrace();
-		}
-		;
+		};
 	}
 
 }
